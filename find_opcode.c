c55fbe8d7f7f5009dd8ca2dcdd619095d37a4d79
Kimberly Wong <kjowong@gmail.com>
2017-05-18 15:35:11 +0000

    Adding opcode function

---
+++ find_opcode.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+/**
+  * find_opcode - finds the opcode
+  * @token: double pointer token
+  * @line_number: number of lines in the file
+  * @head: Pointer to the list
+  * Return: -1 on failure
+  */
+int find_opcode(char **token, size_t line_number, stack_t **head)
+{
+
+	opcode_t opcodes[] = {
+		{"pall", print_node_pall},
+		{"pint", print_first_pint},
+		{"pop", pop_node},
+		{"swap", swap_first_second},
+		{"add", add_first_second},
+		{"nop", nop_node},
+		{NULL, NULL}
+	};
+
+	int retval, i;
+
+	if (!token)
+		return (-1);
+	i = 0;
+	if (strcmp(token[0], "push") == 0)
+	{
+		retval = push_node(head, token[1]);
+		return (retval);
+	}
+	while ((opcodes + i)->opcode != NULL)
+	{
+		if (strcmp(token[0], (opcodes + i)->opcode) == 0)
+		{
+			retval = (opcodes + i)->f(head, line_number);
+			return (retval);
+		}
+		i++;
+	}
+	printf("L%d: unknown instruction %s\n", line_number, token[0])
+	return (-1);
+}

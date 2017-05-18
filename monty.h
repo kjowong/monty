d9b8ea6e0e79e66127a77ff6e0f8d02952548e2e
Kimberly Wong <kjowong@gmail.com>
2017-05-18 12:01:10 +0000

    Adding prototypes

--- monty.h
+++ monty.h
@@ -13,9 +13,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -31,4 +31,12 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+void print_node_pall(stack_t *h);
+void nop_node(stack_t *h);
+stack_t *pop_node_opcode(stack_t **head, size_t line_number);
+stack_t *swap_first_second(stack_t *h, size_t line_number);
+stack_t *add_node_push(stack_t **head, int n);
+void print_first_pint(stack_t *h, size_t line_number);
+stack_t *swap_first_second(stack_t *h, size_t line_number);
+size_t list_len(stack_t *h);
 #endif

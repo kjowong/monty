612f61bddc836d14d592ed0ee0a48f0ea29106fb
Kimberly Wong <kjowong@gmail.com>
2017-05-20 14:16:39 +0000

    changing name of file

---
+++ print_all_pall.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+ * print_node_pall - function that prints all the elements of a list
+ * @h: pointer to the constant stack_t head
+ * @line_num: line number in the file
+ * Return: none
+ */
+void print_node_pall(stack_t **h, __attribute__((unused))unsigned int line_num)
+{
+	stack_t *temp;
+
+	temp = *h;
+
+	if (h == NULL || *h == NULL)
+	{
+		globals.retval = -1;
+		return;
+	}
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+	globals.retval = 0;
+}

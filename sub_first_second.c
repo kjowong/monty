ec6a475b1f161fe7689d53d299e0962eeb8ac2d0
Julija Lee <lee.julija@gmail.com>
2017-05-28 02:43:50 +0000

    adding sub and fixed rotl

--- sub_first_second.c
+++ sub_first_second.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
- * sub_first_second - subtracts the top element of the stack from the second top 
+ * sub_first_second - subtracts the top element of the stack from the 2nd top
  * @h: head pointer
  * @line_number: line number in the file with Monty byte code
  * Return: none
@@ -8,15 +8,17 @@
 void sub_first_second(stack_t **h, unsigned int line_number)
 {
 	stack_t *tmp;
+	int sub;
 
 	if (list_len(*h) < 2)
 	{
-		printf("L%u: sub't add, stack too short\n", line_number);
+		printf("L%u: can't sub, stack too short\n", line_number);
 		globals.retval = -1;
 		return;
 	}
 	tmp = (*h)->next;
-	tmp->n -= (*h)->n;
+	sub = (tmp->n - (*h)->n);
+	temp->n = sub;
 	pop_node(h, line_number);
 	globals.retval = 0;
 }

3ecb7fc5f118864fe08ec6ea62e18de149887782
Julija Lee <lee.julija@gmail.com>
2017-05-28 07:33:13 +0000

    changed pop

--- pop_node.c
+++ pop_node.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 /**
-  * pop_node - function that deletes the head node & returns its data
-  * @head: pointer to a pointer
+  * pop_node - function that deletes the head node
+  * @head: pointer to head
   * @line_number: line number in the file with Monty bytecode
   * Return: none
  */
@@ -9,16 +9,16 @@ void pop_node(stack_t **head, unsigned int line_number)
 {
 	stack_t *temp;
 
-	if (head == NULL || *head == NULL)
+	if (!head || !*head)
 	{
 		printf("L%u: can't pop an empty stack\n", line_number);
 		globals.retval = -1;
 		return;
 	}
 	temp = *head;
-	*head = temp->next;
-	if (temp->next)
-		temp->next->prev = NULL;
+	*head = (*head)->next;
+	if (*head)
+		(*head)->prev = NULL;
 	free(temp);
 	globals.retval = 0;
 }

442cc7d0bfe4546ba7824542d5e86800ad1f28d0
Julija Lee <lee.julija@gmail.com>
2017-05-28 01:26:27 +0000

    added one more  check

--- pop_node.c
+++ pop_node.c
@@ -9,7 +9,7 @@ void pop_node(stack_t **head, unsigned int line_number)
 {
 	stack_t *temp;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL || *head == NULL || list_len(*head) < 1)
 	{
 		printf("L%u: can't pop an empty stack\n", line_number);
 		globals.retval = -1;
